d767ce6455aeacde586fb32c0c8c4c4fc7ef9ed3
ashin19 <ashin19@safeway.com>
2024-10-09 23:51:30 -0700

    done

--- Problem-1.java
+++ Problem-1.java
@@ -1,38 +1,34 @@
+// Time Complexity : O(2^m+n)
+// Space Complexity : O(2^m+n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : used the same logic Jaspinder explained in the class.
+
 class Solution {
-    /**
-    Time Complexity is O(2 ^ m + n)
-    n - length of array and m is the target value.
-    Space - O(n) length of an array
-     */ 
     List<List<Integer>> res;
+    List<Integer> path;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         this.res = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        // helperChoseDontChose(candidates, target, 0,path);
-        helper(candidates, 0, target,path);
+        this.path = new ArrayList<>();
+        forloophelper(candidates, 0, target);
         return res;
     }
 
-    private void helper(int[] candidates, int pivot, int target, List<Integer> path){
+    private void forloophelper(int[] candidates, int pivot, int target){
         //base
         if(target == 0){
-            // res.add(new ArrayList<>(path));
-            res.add(path);
+            res.add(new ArrayList<>(path));
             return;
         }
-
-        if(target < 0) return;
+        if(target < 0 || pivot == candidates.length) return;
 
         //logic
-        //for based recursion
-        for(int i = pivot; i < candidates.length; i++){
-            //If you do not want to do backtracking then create new list and add elements to that list.
-            List<Integer> li = new ArrayList<>(path);
-            // path.add(candidates[i]);
-            li.add(candidates[i]);
-            helper(candidates, i, target - candidates[i], li);
-            // path.remove(path.size() - 1);
-            
+        for(int i = pivot; i < candidates.length; i ++){
+            //action
+            path.add(candidates[i]);
+            //recurse
+            forloophelper(candidates, i, target - candidates[i]);
+            //backtrack
+            path.remove(path.size() - 1);
         }
     }
 }
